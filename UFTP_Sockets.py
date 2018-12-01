9b7652794a20184700fe2ae17e7db36212dfcb78
dmonazah <monazahd@yahoo.com>
2018-12-01 10:54:13 -0500

    Add newline to socket file

--- UFTP_Sockets.py
+++ UFTP_Sockets.py
@@ -5,11 +5,11 @@ def Initialize_Socket():
 
 def Socket_Send(sock,UDP_IP,UDP_PORT,MESSAGE):
     if random.randint(1,10) < 1:
-        print("Packet Dropped: ",MESSAGE)
+        print("\nPacket Dropped: ",MESSAGE)
     else:
         sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
 
 def Socket_Rcv(sock):
     data, addr = sock.recvfrom(16384)
-    print("Socket Received:", data.decode("utf-8"))
+    print("\nSocket Received:", data.decode("utf-8"))
     return(data.decode("utf-8"),addr)
